c9468b34f3774f2ec76ffd7bf88cc96eae9a0b43
Neel Shah <neel.160410107119@gmail.com>
2020-10-08 21:41:59 +0530

    Added /createtabel api for creating user specific tabel

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
 const { connect } = require('./config/db');
 
 connect();
-
-// app.use('/', (req, res) => {
-//   res.send('hello');
-// });
-
-//const jsonParser = bodyParser.json();
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
+
+app.use(cookieParser());
+
+app.use(
+  session({
+    secret: 'secret!',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: true },
+  })
+);
 
 app.use('/', require('./routes/users'));
 
--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,13 @@ const express = require('express');
 const router = express.Router();
 const { db } = require('../config/db');
 const { check, validationResult } = require('express-validator');
+const session = require('express-session');
+const config = require('config');
+const database = config.get('database');
 
 //@route        POST /user
 //@desc         Getting User Specific Table
-//@access       Public
+//@access       Private
 
 router.post(
   '/user',
@@ -31,6 +34,42 @@ router.post(
         : results[0].password === password
         ? res.send(results)
         : res.send('Invalid password');
+    });
+  }
+);
+
+//@route        POST /createtabel
+//@desc         Creating User Specific Table if not exist
+//@access       Private
+
+router.post(
+  '/createtabel',
+  [
+    check('name', 'Enter User Name').not().isEmpty(),
+    check('email', 'Please enter valid email').isEmail(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email } = req.body;
+
+    // let sql = `WITH r AS ( SELECT * FROM information_schema.tables WHERE table_schema = '${database}' AND table_name = '${email}' LIMIT 1 ) SELECT * FROM r UNION ALL SELECT * FROM \`Users\` AND NOT EXISTS ( SELECT * FROM r )`;
+
+    let sql = `SELECT * FROM information_schema.tables WHERE table_schema = '${database}' AND table_name = '${email}' LIMIT 1`;
+
+    let query = db.query(sql, async (err, results) => {
+      if (results.length === 0) {
+        let sqlCreateTabel = `CREATE TABLE \`bge6ne8wbc1bbqr1az2f\`.\`${email}\` ( \`id\` INT NOT NULL AUTO_INCREMENT , \`firstname\` VARCHAR(255) NOT NULL , \`lastname\` VARCHAR(255) NOT NULL , \`phone\` INT(10) NOT NULL , PRIMARY KEY (\`id\`)) ENGINE = InnoDB;`;
+        db.query(sqlCreateTabel, (errTables, resultsTabels) => {
+          console.log(resultsTabels);
+        });
+      }
+      results.length === 0
+        ? res.send('Tabel Created with default columns')
+        : res.send('Tabel exists');
     });
   }
 );
